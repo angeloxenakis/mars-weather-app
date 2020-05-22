3092f26bbec00474ad1c4ceba1cd8714956e293a
angeloxenakis <angeloxenakis@icloud.com>
2020-05-22 10:06:41 -0500

    first draft at a nav bar

--- frontend/src/components/nav/Header.js
+++ frontend/src/components/nav/Header.js
@@ -3,10 +3,17 @@ import React from 'react'
 export function Header() {
     return (
         <div className="nav">
-            <button>Mars</button>
-            <button>Earth</button>
-            <button>My Locations</button>
-            <a href="#">My Profile</a>
+            <div className="nav-btns">
+                <button>Mars</button>
+                <button>Earth</button>
+                <button>Mars Insight</button>
+                <button>My Locations</button>
+            </div>
+            <div className="nav-profile">
+                <a href="#">My Profile</a>
+                <br></br>
+                <a href="#">Log Out</a>
+            </div>
         </div>
     )
 }
