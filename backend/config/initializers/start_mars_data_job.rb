512e6661fa2cae5e65daa8f7ae065069296268f7
angeloxenakis <angeloxenakis@icloud.com>
2020-05-22 11:46:46 -0500

    initial backend fetch setup with Josh

---
+++ backend/config/initializers/start_mars_data_job.rb
@@ -0,0 +1,1 @@
+GetMarsDataJob.perform_later
--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -5,10 +5,23 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'net/http'
+require "open-uri"
 
+# User.create({id: 1, username: "TestUser", password: "123", email: "FakeEmail@email.com"})
 
-User.create({id: 1, username: "TestUser", password: "123", email: "FakeEmail@email.com"})
+# Location.create({user_id: 1, address: "123 Main Street"})
 
-Location.create({user_id: 1, address: "123 Main Street"})
+# MarsWeatherRecord.create({temperature: 4})
 
-MarsWeatherRecord.create({temperature: 4})
+@data = URI.parse("https://api.nasa.gov/insight_weather/?api_key=i9gqXdEKqhWTTCLhNwoJdWmzLyYXQUF96t3bVmfA&feedtype=json&ver=1.0").read
+msg = p JSON.parse(@data)
+byebug
+
+# p "Getting Nasa Data"
+# http = Net::HTTP.new('api.nasa.gov')
+# http.ssl_version = :SSLv23
+# http.use_ssl = true
+# response = http.get('/insight_weather/?api_key=i9gqXdEKqhWTTCLhNwoJdWmzLyYXQUF96t3bVmfA&feedtype=json&ver=1.0')
+# p "Successful http req"
+# p response
