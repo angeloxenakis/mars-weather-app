6fa17a9d279c502baad6a175f092b29d8f5c8ff9
angeloxenakis <angeloxenakis@icloud.com>
2020-05-22 14:26:06 -0500

    fetch data in the back in and filter it into seed file

--- backend/db/migrate/20200521184923_create_mars_weather_records.rb
+++ backend/db/migrate/20200521184923_create_mars_weather_records.rb
@@ -1,7 +1,11 @@
 class CreateMarsWeatherRecords < ActiveRecord::Migration[6.0]
   def change
     create_table :mars_weather_records do |t|
+      t.integer :sol
       t.integer :temperature
+      t.integer :air_pressure
+      t.integer :wind_speed
+      t.string :wind_direction
     end
   end
 end
