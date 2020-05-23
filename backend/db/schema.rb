1f89cec6064da40017f1191f2c00714ad1aa5144
Linxerz <pruittj2000@yahoo.com>
2020-05-23 06:50:01 -0500

    Halfway done, just need to make a signup so I can test my sign in code

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -27,7 +27,7 @@ ActiveRecord::Schema.define(version: 2020_05_21_184923) do
 
   create_table "users", force: :cascade do |t|
     t.string "username"
-    t.string "password"
+    t.string "password_digest"
     t.string "email"
   end
 
