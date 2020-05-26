a6efcd1571173d391f8732185b38b0ebee168ab6
angeloxenakis <angeloxenakis@icloud.com>
2020-05-26 02:28:45 -0500

    Hard coded mars countdown

---
+++ frontend/src/components/mars-weather/MarsCountdown.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export function MarsCountdown() {
+    return (
+        <div className="mars-countdown">
+            <h4>Estimated Countdown to Human Contact on Mars:</h4> 
+            <p><strong>Years:</strong> 9 | <strong>Months:</strong> 7 | <strong>Days:</strong> 4</p>
+        </div>
+    )
+}
