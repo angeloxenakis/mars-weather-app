54fa44d525d050d5ddd2606b9d442522b81c98b4
angeloxenakis <angeloxenakis@icloud.com>
2020-05-26 02:12:56 -0500

    header buttons work, login and signup has quickstyling, footer exists

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,28 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import { Route } from 'react-router';
 import './App.css';
 import { MarsWeatherPanel } from './components/mars-weather/MarsWeatherPanel'
 import { Header } from './components/nav/Header'
 import { Login } from './components/login/Login'
 import { Signup } from './components/login/Signup'
 import { UserWeatherPanel } from './components/user-weather/UserWeatherPanel'
-import { BrowserRouter } from 'react-router-dom'
-import { Route } from 'react-router'
+import { Footer } from './components/nav/Footer'
+
 
 function App() {
 	return (
 		<div className="App">
 			<Header/>
-			<BrowserRouter>
-				<Route exact path="/mars-weather" component={MarsWeatherPanel}/>
-				<Route path="/earth-weather" components={UserWeatherPanel}/>
-				<Route path="/login" component={Login}/>
-				<Route path="/signup" component={Signup}/>
-			</BrowserRouter>
+			<div className="container">
+				<BrowserRouter>
+					<Route exact path="/mars-weather" component={MarsWeatherPanel}/>
+					<Route path="/earth-weather" components={UserWeatherPanel}/>
+					<Route path="/login" component={Login}/>
+					<Route path="/signup" component={Signup}/>
+				</BrowserRouter>
+			</div>
+			<Footer/>
 		</div>
 	);
 }
---
+++ frontend/src/components/nav/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export function Footer() {
+    return (
+        <div className="footer-container">
+            <div className="footer">
+                <p>Mars Weather App was created by Angelo Xenakis and Johnathan Pruitt, and uses the Nasa Insight and OpenWeather APIs.</p>
+            </div>
+        </div>
+    )
+}
