36d2972c31329ccc1c9d73a1533ef2abdce65f63
Linxerz <pruittj2000@yahoo.com>
2020-05-26 11:08:40 -0500

    Added better weather functionality

--- frontend/src/components/user-weather/CurrentDayInfo.js
+++ frontend/src/components/user-weather/CurrentDayInfo.js
@@ -1,41 +1,57 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import {CurrentDayTemperature} from './CurrentDayTemperature.js'
 import {CurrentDayStatus} from './CurrentDayStatus.js'
 
+let useComponentDidMount = (callback) => {
+    useEffect(callback, [])
+}
+
 export function CurrentDayInfo(props) {
+
+    //GET USERS CURRENT CITY
+
+
+    let [currentDay, setCurrentDay] = useState(null)
+
+    useComponentDidMount( () => {
+        fetch('https://api.openweathermap.org/data/2.5/weather?q=Houston&appid=b2155d7bcc9acd90bde0e1c32ab5d85e')
+            .then( resp => resp.json() )
+            .then( currentDay => {(
+                setCurrentDay(currentDay)
+                )} ) 
+    })
    
 
-    let currentDay = props.currentDay
-    let currentDate = new Date(props.currentDay.dt_txt)
 
-    let status = currentDay.weather[0].main
 
     console.log(currentDay)
-    console.log(currentDate)
-    console.log(status)
-    
 
-    let kelvinTemp = props.currentDay.main.temp
+    
     let farenTemp = null
 
-    let weatherInfo = props.currentDay.weather.main
-    let weatherDescription = props.currentDay.weather.description
 
     let convertKtoF = (k) => {
-        farenTemp = Math.round(((k - 273.15)*(9/5)+32))
+        return Math.round(((k - 273.15)*(9/5)+32))
     }
 
-    convertKtoF(kelvinTemp)
 
-    return(
+
+    if(currentDay === null){
+        return(
+            <h1>Loading Current Weather Info</h1>
+        )
+    }
+    else{
+        let status = currentDay.weather[0].main
+        return(
         <div className="weather-info">
             <div className="main-info">
                 
-                <CurrentDayTemperature temperature = {farenTemp}/>
+                <CurrentDayTemperature temperature = {convertKtoF(currentDay.main.temp)}/>
             </div>
             <div className="side-info">
                 <CurrentDayStatus status = {status}/>
             </div>
         </div>
-    )
+    )}
 }
--- frontend/src/components/user-weather/CurrentDayStatus.js
+++ frontend/src/components/user-weather/CurrentDayStatus.js
@@ -10,7 +10,43 @@ export function CurrentDayStatus(props) {
                 <strong><p>Cloudy</p></strong>
             </div>
         )
+
+    else if(props.status === "Rain")
+        return (
+            <div className="season-panel">
+                <h4>Status</h4>
+                <img src="https://img.icons8.com/ios/50/000000/clouds.png"/>
+                <strong><p>Rainy</p></strong>
+            </div>
+        )
     
+    else if(props.status === "Drizzle")
+        return (
+            <div className="season-panel">
+                <h4>Status</h4>
+                <img src="https://img.icons8.com/ios/50/000000/clouds.png"/>
+                <strong><p>Rainy</p></strong>
+            </div>
+        )
+    
+    else if(props.status === "Thunderstorm")
+        return (
+            <div className="season-panel">
+                <h4>Status</h4>
+                <img src="https://img.icons8.com/ios/50/000000/clouds.png"/>
+                <strong><p>Rainy</p></strong>
+            </div>
+        )
+
+    else if(props.status === "Snow")
+        return (
+            <div className="season-panel">
+                <h4>Status</h4>
+                <img src="https://img.icons8.com/ios/50/000000/clouds.png"/>
+                <strong><p>Rainy</p></strong>
+            </div>
+        )
+
     else{
         return(
             <div className="season-panel">
