d950d6ed42c57b8fc7502b6a7c3386fa2b7c8cc4
angeloxenakis <angeloxenakis@icloud.com>
2020-05-27 14:17:23 -0500

    hard coded skeleton of weather comparison

---
+++ frontend/src/components/compare-weather/CompareMars.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+export function CompareMars() {
+    return (
+        <div className="compare-mars-container">
+            <h2>Mars Weather</h2>
+            <div className="compare-mars-info">
+                <h4>Temperature:</h4>
+                <p>-64°C</p>
+                <hr></hr><br></br>
+                <h4>Wind:</h4>
+                <p>Speed: 4 mph</p>
+                <p>Direction: NW</p>
+                <hr></hr><br></br>
+                <h4>Air Pressure:</h4>
+                <p>-64°C</p>
+                <hr></hr><br></br>
+                <h4>Season:</h4>
+                <p>Summer</p>
+                <hr></hr><br></br>
+            </div>
+        </div>
+    )
+}
---
+++ frontend/src/components/compare-weather/CompareWeather.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import { CompareMars } from './CompareMars'
+import { CompareEarth } from './CompareEarth'
+import { MarsCountdown } from '../mars-weather/MarsCountdown'
+import insight from "../../assets/red-insight.svg"
+
+
+export function CompareWeather() {
+
+    let currentDate = new Date().toDateString();
+
+    return (
+        <div>
+            <div className="compare-container">
+                <div className="weather-title"><img width="56px" height="56px" src={insight}/><h1>Compare Weather</h1></div>
+                <p className="day-info"><strong>Earth Date: </strong> {currentDate} | <strong>Mars SOL: </strong>527</p>
+                <div className="compare-panels">
+                    <div>
+                        <CompareMars/>
+                    </div>
+                    <div>
+                        <CompareEarth/>
+                    </div>
+                </div>
+            </div>
+            <MarsCountdown/>
+        </div>
+
+    )
+}
